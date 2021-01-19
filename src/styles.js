ef0f56973a9e52a6ea331bd0f3b6134dc2263e3a
Denin Grcic <deningrcic@MacBook-Pro.local>
2021-01-18 16:44:50 -0800

    Minor styling changes

--- src/styles.js
+++ src/styles.js
@@ -15,7 +15,6 @@ export default makeStyles((theme) => ({
   image: {
     marginLeft: "15px",
   },
-
   [theme.breakpoints.down("sm")]: {
     mainContainer: {
       flexDirection: "column-reverse",
