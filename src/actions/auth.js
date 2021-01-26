5848a55966e6d4eb53f7b699660129b92f9e5856
Denin Grcic <deningrcic@MacBook-Pro.local>
2021-01-25 22:09:11 -0800

    Created auth actions

---
+++ src/actions/auth.js
@@ -0,0 +1,21 @@
+import { AUTH } from "../constants/actionTypes";
+
+import * as api from "../api";
+
+export const signin = (formData, history) => async (dispatch) => {
+  try {
+    //login user
+    history.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = (formData, history) => async (dispatch) => {
+  try {
+    //sign up user
+    history.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
