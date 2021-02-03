00d9c6da12ca1e403df0279cc5bdb5ca04c4ac2f
Denin Grcic <deningrcic@MacBook-Pro.local>
2021-02-02 21:29:02 -0800

    Added tokens

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,27 @@
 import axios from "axios";
 
-const url = "https://memories-project4.herokuapp.com/posts";
+const API = axios.create({
+  baseURL: "http://localhost:5000",
+});
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+
+  return req;
+});
+
+//"https://memories-project4.herokuapp.com/posts"
+
+export const fetchPosts = () => API.get("/posts");
+export const createPost = (newPost) => API.post("/posts", newPost);
 export const updatePost = (id, updatedPost) =>
-  axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+  axios.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`/posts/${id}`);
+export const likePost = (id) => axios.patch(`/posts/${id}/likePost`);
+
+export const signIn = (formData) => API.post("/user/signin", formData);
+export const signUp = (formData) => API.post("/user/signup", formData);
