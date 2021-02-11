ead6e89b1efc7611e2e85d9e2a90714f576beea1
Denin Grcic <deningrcic@MacBook-Pro.local>
2021-02-10 19:34:46 -0800

    Added token expiration logic

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useHistory, useLocation } from "react-router-dom";
 import { AppBar, Typography, Toolbar, Avatar, Button } from "@material-ui/core";
 import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
 import useStyles from "./styles";
 import memories from "../../images/memories.png";
 
@@ -11,6 +12,7 @@ const NavBar = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const location = useLocation();
+
   const logout = () => {
     dispatch({ type: "LOGOUT" });
     history.push("/auth");
@@ -19,6 +21,12 @@ const NavBar = () => {
 
   useEffect(() => {
     const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
 
     setUser(JSON.parse(localStorage.getItem("profile")));
   }, [location]);
