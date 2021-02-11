fdf14b00b23675ca8ad4101b6fdbee9fbbfab7cf
Denin Grcic <deningrcic@MacBook-Pro.local>
2021-02-10 19:06:45 -0800

    Added clear event handler and minor refactoring

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
-import FileBase from "react-file-base64";
 import { useDispatch, useSelector } from "react-redux";
-import useStyles from "./styles";
-import { createPost, updatePost } from "../../actions/posts";
+import FileBase from "react-file-base64";
 
-//Get current post id
+import { createPost, updatePost } from "../../actions/posts";
+import useStyles from "./styles";
+
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
     title: "",
@@ -14,47 +14,45 @@ const Form = ({ currentId, setCurrentId }) => {
     selectedFile: "",
   });
   const post = useSelector((state) =>
-    currentId ? state.posts.find((p) => p._id === currentId) : null
+    currentId ? state.posts.find((message) => message._id === currentId) : null
   );
-  const classes = useStyles();
   const dispatch = useDispatch();
+  const classes = useStyles();
   const user = JSON.parse(localStorage.getItem("profile"));
 
   useEffect(() => {
     if (post) setPostData(post);
   }, [post]);
 
-  const handleSubmit = (e) => {
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({ title: "", message: "", tags: "", selectedFile: "" });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (currentId === 0) {
       dispatch(createPost({ ...postData, name: user?.result?.name }));
+      clear();
     } else {
       dispatch(
         updatePost(currentId, { ...postData, name: user?.result?.name })
       );
+      clear();
     }
-    clear();
   };
 
   if (!user?.result?.name) {
     return (
       <Paper className={classes.paper}>
         <Typography variant="h6" align="center">
-          Please Sign in to create your own memories and like other's memories.
+          Please Sign In to create your own memories and like other's memories.
         </Typography>
       </Paper>
     );
   }
 
-  const clear = () => {
-    setCurrentId(null);
-    setPostData({
-      title: "",
-      message: "",
-      tags: "",
-      selectedFile: "",
-    });
-  };
   return (
     <Paper className={classes.paper}>
       <form
@@ -64,7 +62,7 @@ const Form = ({ currentId, setCurrentId }) => {
         onSubmit={handleSubmit}
       >
         <Typography variant="h6">
-          {currentId ? "Editing" : "Creating"} a Memory
+          {currentId ? `Editing "${post.title}"` : "Creating a Memory"}
         </Typography>
         <TextField
           name="title"
@@ -72,29 +70,29 @@ const Form = ({ currentId, setCurrentId }) => {
           label="Title"
           fullWidth
           value={postData.title}
-          onChange={(e) => {
-            setPostData({ ...postData, title: e.target.value });
-          }}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
         />
         <TextField
           name="message"
           variant="outlined"
           label="Message"
           fullWidth
+          multiline
+          rows={4}
           value={postData.message}
-          onChange={(e) => {
-            setPostData({ ...postData, message: e.target.value });
-          }}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
         />
         <TextField
           name="tags"
           variant="outlined"
-          label="Tags"
+          label="Tags (coma separated)"
           fullWidth
           value={postData.tags}
-          onChange={(e) => {
-            setPostData({ ...postData, tags: e.target.value.split(",") });
-          }}
+          onChange={(e) =>
+            setPostData({ ...postData, tags: e.target.value.split(",") })
+          }
         />
         <div className={classes.fileInput}>
           <FileBase
